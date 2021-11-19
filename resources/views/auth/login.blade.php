8b0e55ff19366e25aac81c14e8ee89fea13f7791
andrew21-mch <nfonandrew73@gmail.com>
2021-11-19 05:28:31 +0100

    updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
 
                         <div class="form-group row justify-content-center">
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                               <label for="email">Email</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -65,7 +65,7 @@
                         </div>
 
                         <div class="form-group row justify-content-center">
-                            <div class="col-md-6"><br>
+                            <div class="col-md-8"><br>
                               <label for="password">Password</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -78,7 +78,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-3">
+                            <div class="col-md-8 offset-md-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="remember">
