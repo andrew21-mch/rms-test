d7c71e7d995fa5b0ee8e5492b846fd25db4a1de4
andrew21-mch <nfonandrew73@gmail.com>
2021-11-20 02:21:13 +0100

    fixed bug

--- resources/views/class/form1.blade.php
+++ resources/views/class/form1.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="class">
   <br><br><br><br>
-  
+
         <div class="container" style="font-size:12px">
         <center>
           <div class="row col-md-12">
@@ -41,65 +41,57 @@
       </center>
   <table id="customers">
     <div class="row"><h1><center>Class List Form 1</center></h1></div>
-      @if(Session::get('user1')==200)
-    <tr>
-      <th>Student First Name</th>
-      <th>Student Last Name</th>
-      <th>Gender</th>
-      <th>DOB</th>
-      <th>Action</th>
-      <th>A</th>
-      <th>B</th>
-    </tr>
+    @if(Session::get('user1')==200)
+      <tr>
+        <th>Student First Name</th>
+        <th>Student Last Name</th>
+        <th>Gender</th>
+        <th>DOB</th>
+        <th>Action</th>
+      </tr>
 
-    @foreach ($data as $user)
-    @if($user->class_id == 1)
-    <tr>
-      <td>{{$user->first_name}}</td>
-      <td>{{$user->last_name}}</td>
-      <td>{{$user->gender}}</td>
-      <td>{{$user->date_of_birth}}</td>
-      <td></td>
-      <td></td>
+      @foreach ($data as $user)
+        @if($user->class_id == 1)
+        <tr>
+          <td>{{$user->first_name}}</td>
+          <td>{{$user->last_name}}</td>
+          <td>{{$user->gender}}</td>
+          <td>{{$user->date_of_birth}}</td>
+          <td></td>
+          <td></td>
 
-      <td>
-        <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
-        <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
-        <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
-      </td>
-    </tr>
+          <td>
+            <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
+            <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
+            <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
+          </td>
+        </tr>
+        @endif
+      @endforeach
+    @else
+      <tr>
+        <th>Student First Name</th>
+        <th>Student Last Name</th>
+        <th>Gender</th>
+        <th>DOB</th>
+        <th>Action</th>
+      </tr>
 
-    @endif
-    @endforeach
-  @else
-
-  <tr>
-    <th>Student First Name</th>
-    <th>Student Last Name</th>
-    <th>Gender</th>
-    <th>DOB</th>
-    <th>A</th>
-    <th>B</th>
-    <th>Action</th>
-  </tr>
-
-  @foreach ($data as $user)
-  @if($user->class_id == 1)
-  <tr>
-    <td>{{$user->first_name}}</td>
-    <td>{{$user->last_name}}</td>
-    <td>{{$user->gender}}</td>
-    <td>{{$user->date_of_birth}}</td>
-    <td></td>
-    <td></td>
-    
-    <td>
-      <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
-      <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
-      <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
-    </td>
-  </tr>
-  @endif
+      @foreach ($data as $user)
+        @if($user->class_id == 1)
+          <tr>
+            <td>{{$user->first_name}}</td>
+            <td>{{$user->last_name}}</td>
+            <td>{{$user->gender}}</td>
+            <td>{{$user->date_of_birth}}</td>
+            <td>
+              <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
+              <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
+              <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
+            </td>
+        </tr>
+        @endif
   @endforeach
   @endif
-    @endsection
+</div>
+@endsection
--- resources/views/class/form4.blade.php
+++ resources/views/class/form4.blade.php
@@ -2,33 +2,96 @@
 @section('content')
 <div class="class">
   <br><br><br><br>
+
+        <div class="container" style="font-size:12px">
+        <center>
+          <div class="row col-md-12">
+            <table>
+              <tr>
+                <td colspan="5">
+                    <center style="width:230px; float:left">REPUBLIC DU CAMEROUN <br>
+                    Paix-Travail-Partrie <br>
+                    MINISTRE TO ENSEIGMENT SECONDAIRE <br>
+                    DELEGATION REGIONAL DU NORHT WEST  <br>
+                    SAINT FRANCISCA MULTILINGUAL <br>
+                    EDUCATIONAL COMPLEX</center>
+                  </td>
+
+                  <td colspan="2">
+                    <center>
+                      <span class="iconify" data-icon="icons8:student" data-width="100" data-height="100"></span>
+                    </center>
+                </td>
+
+                <td colspan="5">
+
+                    <center style="width:230px; float: right">REPUBLIC DU CAMEROUN <br>
+                    Paix-Travail-Partrie <br>
+                    MINISTRE TO ENSEIGMENT SECONDAIRE <br>
+                    DELEGATION REGIONAL DU NORHT WEST  <br>
+                    SAINT FRANCISCA MULTILINGUAL <br>
+                    EDUCATIONAL COMPLEX</center>
+                  </td>
+
+              </div>
+              </tr>
+
+            </table>
+
+      </center>
   <table id="customers">
-    <tr>
-      <h2>Form 4</h2>
-    </tr>
-    <tr>
-      <th>Student First Name</th>
-      <th>Student Last Name</th>
-      <th>Gender</th>
-      <th>DOB</th>
-      <th>Action</th>
-    </tr>
+    <div class="row"><h1><center>Class List Form 4</center></h1></div>
+    @if(Session::get('user1')==200)
+      <tr>
+        <th>Student First Name</th>
+        <th>Student Last Name</th>
+        <th>Gender</th>
+        <th>DOB</th>
+        <th>Action</th>
+      </tr>
 
-    @foreach ($data as $user)
-    @if($user->class_id == 4)
-    <tr>
-      <td>{{$user->first_name}}</td>
-      <td>{{$user->last_name}}</td>
-      <td>{{$user->gender}}</td>
-      <td>{{$user->date_of_birth}}</td>
-      <td>
-        <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
-        <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
-        <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
-      </td>
+      @foreach ($data as $user)
+        @if($user->class_id == 4)
+        <tr>
+          <td>{{$user->first_name}}</td>
+          <td>{{$user->last_name}}</td>
+          <td>{{$user->gender}}</td>
+          <td>{{$user->date_of_birth}}</td>
+          <td></td>
+          <td></td>
 
-    </tr>
-    @endif
-    @endforeach
-    @endif
-    @endsection
+          <td>
+            <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
+            <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
+            <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
+          </td>
+        </tr>
+        @endif
+      @endforeach
+    @else
+      <tr>
+        <th>Student First Name</th>
+        <th>Student Last Name</th>
+        <th>Gender</th>
+        <th>DOB</th>
+        <th>Action</th>
+      </tr>
+
+      @foreach ($data as $user)
+        @if($user->class_id == 4)
+          <tr>
+            <td>{{$user->first_name}}</td>
+            <td>{{$user->last_name}}</td>
+            <td>{{$user->gender}}</td>
+            <td>{{$user->date_of_birth}}</td>
+            <td>
+              <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
+              <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
+              <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
+            </td>
+        </tr>
+        @endif
+  @endforeach
+  @endif
+</div>
+@endsection
