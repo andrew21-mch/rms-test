79134fb19bc43b068306383132586b244dfc2745
andrew21-mch <nfonandrew73@gmail.com>
2021-11-20 04:11:13 +0100

    tried different style

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::view('/register', 'auth.register');
 Route::view('/adminRegisterTeacher', 'auth.admin_register');
 Route::view('/allresult', 'marks.allresult');
 Route::view('/search_tips', 'search_tips');
-
+Route::view('/conact_us','contact');
 
 Route::view('/Action/registerStudent', 'registerStudent');
 Route::view('/reportCard','reportCard');
