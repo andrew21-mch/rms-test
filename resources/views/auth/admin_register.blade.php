97b08d7ffa6f5c5835d73df4cb57c9742fb4b61b
andrew21-mch <nfonandrew73@gmail.com>
2021-11-20 03:46:14 +0100

    tried different style

--- resources/views/auth/admin_register.blade.php
+++ resources/views/auth/admin_register.blade.php
@@ -1,177 +1,190 @@
-@extends('layouts.layout')
-@section('content')
-<style media="screen">
-  body{
-    height: 100%;
-    }
-    .form-group{
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+      <link rel="stylesheet" href="{{URL::asset('css/report.css')}}">
+      <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style media="screen">
+    body{
+      height: 100%;
+      }
+      .container{
+        margin-top: 3%;
+      }
+      .form-group{
+        width: 100%;
+        height: 30%;
+      }
+      .col-md-10{
       width: 100%;
-      height: 30%;
-    }
-    .col-md-10{
-    width: 100%;
-    }
-    .card-body{
-      width: 100%;
-      height: 60%; }
-    .row{
-      width: 98%;
-    }
-  </style>
-<div class="container" style="padding:120px">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-        <div class="card-header"><center><h1>Register Teacher</h1></center></div>
+      }
+      .card-body{
+        width: 100%;
+        height: 60%; }
+      .row{
+        width: 98%;
+      }
+    </style>
+  </head>
+  <body style="background-image: url('images/cast.jpeg'); background-repeat:no-repeat; background-size: cover; background-position:fixed">
+    <div class="container" style="padding:60">
+      <div class="row justify-content-center">
+          <div class="col-md-10">
+              <div class="card">
+          <div class="card-header"><center><h1>Register Teacher</h1></center></div>
 
-        <div class="card-body">
-          <form action="/create_account" method="post">
-            @csrf
-                <div class="form-group row justify-content-center">
-                      <div class="col-md-6">
-                        <label for="name" >First Name</label>
-                          <input id="fname" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="name" autofocus>
+          <div class="card-body">
+            <form action="/create_account" method="post">
+              @csrf
+                  <div class="form-group row justify-content-center">
+                        <div class="col-md-6">
+                          <label for="name" >First Name</label>
+                            <input id="fname" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="name" autofocus>
 
-                          @error('fname')
+                            @error('fname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="col-md-6">
+                          <label for="lname" >Last Name</label>
+                            <input id="lname" type="text" class="form-control @error('lname') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="lname">
+
+                            @error('lname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                  </div>
+                  <div class="form-group row justify-content-center">
+
+                  </div>
+                  <div class="form-group row justify-content-center">
+
+                      <div class="col-md-12">
+                        <label for="email">Email</label>
+                          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                          @error('email')
                               <span class="invalid-feedback" role="alert">
                                   <strong>{{ $message }}</strong>
                               </span>
                           @enderror
                       </div>
-                      <div class="col-md-6">
-                        <label for="lname" >Last Name</label>
-                          <input id="lname" type="text" class="form-control @error('lname') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="lname">
+                  </div>
 
-                          @error('lname')
+                  @if(Session::get('user1')==200)
+                  <div class="form-group row justify-content-center">
+                      <div class="col-md-6">
+                        <label for="phone" >{{ __('Phone Number') }}</label>
+                          <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+
+                          @error('phone')
                               <span class="invalid-feedback" role="alert">
                                   <strong>{{ $message }}</strong>
                               </span>
                           @enderror
                       </div>
-                </div>
-                <div class="form-group row justify-content-center">
-
-                </div>
-                <div class="form-group row justify-content-center">
-
-                    <div class="col-md-12">
-                      <label for="email">Email</label>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                @if(Session::get('user1')==200)
-                <div class="form-group row justify-content-center">
                     <div class="col-md-6">
-                      <label for="phone" >{{ __('Phone Number') }}</label>
-                        <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+                      <label for="role" >{{ __('Role') }}</label>
+                        <select class="form-control" name="">
+                          <option value="">Select Role</option>
+                          <option value="100">Teacher</option>
+                          <option value="101">HOD</option>
+                          <option value="200">Admin</option>
+                        </select>
 
-                        @error('phone')
+                        @error('role')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
                     </div>
-                  <div class="col-md-6">
-                    <label for="role" >{{ __('Role') }}</label>
-                      <select class="form-control" name="">
-                        <option value="">Select Role</option>
-                        <option value="100">Teacher</option>
-                        <option value="101">HOD</option>
-                        <option value="200">Admin</option>
-                      </select>
+                    </div>
 
-                      @error('role')
+                  @else
+                  <div class="form-group row justify-content-center">
+                      <div class="col-md-12">
+                        <label for="phone" >{{ __('Phone Number') }}</label>
+                          <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+
+                          @error('phone')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
+                  @endif
+                  <div class="form-group row justify-content-center">
+                      <div class="col-md-6">
+                        <label for="subject" >{{ __('Subject') }}</label>
+                        <select class="form-control" name="subject">
+                          <option value="">Select Subject</option>
+                          @foreach($subject as $subject)
+                          <option value="{{$subject->id}}">{{$subject->name}}</option>
+                          @endforeach
+                        </select>
+                            @error('subject')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                      <div class="col-md-6">
+                        <label for="password-confirm" >{{ __('Option') }}</label>
+                        <select class="form-control" name="option">
+                          <option value="">Select Option</option>
+                          @foreach($option as $option)
+                          <option value="{{$option->id}}">{{$option->name}}</option>
+                          @endforeach
+                        </select>
+                        @error('option')
                           <span class="invalid-feedback" role="alert">
                               <strong>{{ $message }}</strong>
                           </span>
                       @enderror
+                      </div>
                   </div>
+
+
+
+                  <div class="form-group row justify-content-center">
+                      <div class="col-md-6 mt-1">
+                        <label for="password" >{{ __('Password') }}</label>
+                          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                          @error('password')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                      <div class="col-md-6 mt-1">
+                        <label for="password-confirm" >{{ __('Confirm Password') }}</label>
+                          <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                      </div>
                   </div>
 
-                @else
-                <div class="form-group row justify-content-center">
-                    <div class="col-md-12">
-                      <label for="phone" >{{ __('Phone Number') }}</label>
-                        <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
 
-                        @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-                @endif
-                <div class="form-group row justify-content-center">
-                    <div class="col-md-6">
-                      <label for="subject" >{{ __('Subject') }}</label>
-                      <select class="form-control" name="subject">
-                        <option value="">Select Subject</option>
-                        @foreach($subject as $subject)
-                        <option value="{{$subject->id}}">{{$subject->name}}</option>
-                        @endforeach
-                      </select>
-                          @error('subject')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="col-md-6">
-                      <label for="password-confirm" >{{ __('Option') }}</label>
-                      <select class="form-control" name="option">
-                        <option value="">Select Option</option>
-                        @foreach($option as $option)
-                        <option value="{{$option->id}}">{{$option->name}}</option>
-                        @endforeach
-                      </select>
-                      @error('option')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    </div>
-                </div>
+                  <div class="form-group row justify-content-center">
 
+                  </div>
 
-
-                <div class="form-group row justify-content-center">
-                    <div class="col-md-6 mt-1">
-                      <label for="password" >{{ __('Password') }}</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="col-md-6 mt-1">
-                      <label for="password-confirm" >{{ __('Confirm Password') }}</label>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                    </div>
-                </div>
-
-
-                <div class="form-group row justify-content-center">
-
-                </div>
-
-                <div class="form-group row justify-content-center mb-0">
-                    <div class="col-md-6 offset-md-4 " style="margin-top:15px">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Register') }}
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-@endsection
+                  <div class="form-group row justify-content-center mb-0">
+                      <div class="col-md-6 offset-md-4 " style="margin-top:15px">
+                          <button type="submit" class="btn btn-primary">
+                              {{ __('Register') }}
+                          </button>
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
+    </body>
+</html>
