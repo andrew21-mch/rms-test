055fafd3994c342eb0c18f6cdfb6f6d37285b104
andrew21-mch <nfonandrew73@gmail.com>
2021-11-28 15:16:12 +0100

    clearing memory

--- resources/views/class/form1.blade.php
+++ resources/views/class/form1.blade.php
@@ -41,13 +41,11 @@
       </center>
   <table id="customers">
     <div class="row"><h1><center>Class List Form 1</center></h1></div>
-    @if(Session::get('user1')==200)
       <tr>
         <th>Student First Name</th>
         <th>Student Last Name</th>
         <th>Gender</th>
         <th>DOB</th>
-        <th>Action</th>
       </tr>
 
       @foreach ($data as $user)
@@ -57,41 +55,8 @@
           <td>{{$user->last_name}}</td>
           <td>{{$user->gender}}</td>
           <td>{{$user->date_of_birth}}</td>
-          <td></td>
-          <td></td>
-
-          <td>
-            <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
-            <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
-            <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
-          </td>
         </tr>
         @endif
       @endforeach
-    @else
-      <tr>
-        <th>Student First Name</th>
-        <th>Student Last Name</th>
-        <th>Gender</th>
-        <th>DOB</th>
-        <th>Action</th>
-      </tr>
-
-      @foreach ($data as $user)
-        @if($user->class_id == 1)
-          <tr>
-            <td>{{$user->first_name}}</td>
-            <td>{{$user->last_name}}</td>
-            <td>{{$user->gender}}</td>
-            <td>{{$user->date_of_birth}}</td>
-            <td>
-              <a href="view/{{$user->id}}"><span class="iconify" data-icon="emojione-v1:eye"></span></a>
-              <a href="edit/{{$user->id}}"><span class="iconify" data-icon="carbon:add-filled"></span></a>
-              <a href="delete/{{$user->id}}"><span class="iconify" data-icon="bi:trash-fill"></span></a>
-            </td>
-        </tr>
-        @endif
-  @endforeach
-  @endif
 </div>
 @endsection
