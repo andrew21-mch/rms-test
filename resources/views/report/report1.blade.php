ab8346f79ed27fe605b030ecf0d5e3d346b92e84
andrew21-mch <nfonandrew73@gmail.com>
2022-01-17 11:51:09 +0100

    register page and style

--- resources/views/report/report1.blade.php
+++ resources/views/report/report1.blade.php
@@ -242,9 +242,10 @@
           </tr>
         </table>
   </center>
+  <span style="float:left; margin-left:20%;margin-top:2%" ><i class="fa fa-print fa-2x" aria-hidden="true" OnClick="CallPrint(this.value)" style="cursor:pointer" title="Print the Report"></i></span>
+
   </div>
 </div>
-      <span style="float:left; margin-left:20%" ><i class="fa fa-print fa-2x" aria-hidden="true" OnClick="CallPrint(this.value)" style="cursor:pointer" title="Print the Report"></i></span>
         @else
       <div class="row justify-content-center col-md-12">
         <div class="alert alert-primary mt-4" role="alert" style="text-align:center">
