0cd91df6832ae6418abc7ab07b0f04e0e715b153
andrew21-mch <nfonandrew73@gmail.com>
2022-02-04 17:29:11 +0100

    add subject code

--- app/Http/Controllers/reportcardController.php
+++ app/Http/Controllers/reportcardController.php
@@ -17,7 +17,7 @@ class reportcardController extends Controller
       ->where('rms_students.id','=',$id)
       ->orderBy('coefficient', 'desc')
       ->orderBy('category', 'ASC')
-      ->orderBy('rms_subjects', 'ASC')
+      //->orderBy('rms_subjects', 'ASC')
       ->get();
 
       return view('report.report1', ['data1'=>$data1]);
@@ -31,7 +31,7 @@ class reportcardController extends Controller
       ->where('rms_students.id','=',$id)
       ->orderBy('coefficient', 'desc')
       ->orderBy('category', 'ASC')
-      ->orderBy('rms_subjects', 'ASC')
+      //->orderBy('rms_subjects', 'ASC')
       ->get();
 
       return view('report.report2', ['data1'=>$data1]);
@@ -45,7 +45,7 @@ class reportcardController extends Controller
       ->where('rms_students.id','=',$id)
       ->orderBy('coefficient', 'desc')
       ->orderBy('category', 'ASC')
-      ->orderBy('rms_subjects', 'ASC')
+      //->orderBy('rms_subjects', 'ASC')
       ->get();
 
       return view('report.report3', ['data1'=>$data1]);
