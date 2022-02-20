c11674c37276b5537df16de51a10824084f9e704
nfon andrew <58951422+andrew21-mch@users.noreply.github.com>
2022-02-20 13:06:39 +0100

    Mail readability
    Updated mail readability

--- resources/views/contactmail.blade.php
+++ resources/views/contactmail.blade.php
@@ -1,4 +1,4 @@
-<div style="background-color: rgb(7, 21, 46); color:white; height:50%; ">
+<div style="background-color: rgb(7, 21, 46); color:white; height:50%; margin:3% ">
     <h3>You have a new message from {{$email->name}}:</h3>
     <b>Message:</b>
     <p>
